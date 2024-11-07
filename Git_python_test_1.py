8520c9182bd9824422fe85d73013edd5863c7cf0
Ellen McCullough <ellen.mccullough@kaplan.com>
2024-11-07 11:54:49 +0000

    My first commit to github

---
+++ Git_python_test_1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  7 11:50:50 2024
+
+@author: ellen
+"""
+
+
+"Hello this is my first python file I will be using git with to do version control"
+
+
+def git_test_func():
+    return "hi GIt"
+
+
